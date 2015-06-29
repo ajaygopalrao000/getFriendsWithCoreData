0f39ed9fb338b515d42c6b481813466708dbc143
ajaygopalrao000 <ajay.gopalrao000@gmail.com>
2015-06-28 23:50:50 -0700

    Initial Commit

---
+++ getFriendsWithCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  getFriendsWithCoreData
+//
+//  Created by Gopal Rao on 6/28/15.
+//  Copyright (c) 2015 Gopal Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
